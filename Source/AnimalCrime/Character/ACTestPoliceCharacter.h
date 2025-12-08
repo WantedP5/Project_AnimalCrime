2301f588df2fe2362c829ebead90cfa0d213dc27
Sunwoo Lee <sunwoo9078@gmail.com>
2025-12-09 01:39:20 +0900

    add: 경찰 캐릭터 코드 추가
    베이스에서 상속받음

---
+++ Source/AnimalCrime/Character/ACTestPoliceCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ACCharacter.h"
+#include "ACTestPoliceCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMALCRIME_API AACTestPoliceCharacter : public AACCharacter
+{
+	GENERATED_BODY()
+	
+};
