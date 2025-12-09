da5601a3f803fabc2dea775d9e0b9b60a3f3118a
Sunwoo Lee <70701711+Sunnew-Lee@users.noreply.github.com>
2025-12-09 16:47:46 +0900

    add: Bat weapon added
    - 오른손에 소켓 -> 방망이 부착

--- Source/AnimalCrime/Character/ACTestPoliceCharacter.h
+++ Source/AnimalCrime/Character/ACTestPoliceCharacter.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -14,4 +14,11 @@ class ANIMALCRIME_API AACTestPoliceCharacter : public AACCharacter
 {
 	GENERATED_BODY()
 	
+public:
+	AACTestPoliceCharacter();
+
+protected:
+	//!< 메쉬 컴포넌트
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	TObjectPtr<class USkeletalMeshComponent> WeaponMesh;
 };
