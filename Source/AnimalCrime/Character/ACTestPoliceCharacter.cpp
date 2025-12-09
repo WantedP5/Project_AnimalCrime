c3b0b645ba06420f872ecd903c0ac5c73bfbe218
Sunwoo Lee <70701711+Sunnew-Lee@users.noreply.github.com>
2025-12-09 16:47:46 +0900

    add: Bat weapon added
    - 오른손에 소켓 -> 방망이 부착

--- Source/AnimalCrime/Character/ACTestPoliceCharacter.cpp
+++ Source/AnimalCrime/Character/ACTestPoliceCharacter.cpp
@@ -1,5 +1,18 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Character/ACTestPoliceCharacter.h"
 
+AACTestPoliceCharacter::AACTestPoliceCharacter()
+{
+	// Weapon Component
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Weapon"));
+	//todo: Load Weapon through Inventory
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> WeaponMeshRef(
+		TEXT("/Game/Weapons_Free/Meshes/SKM_baseball_bat_001.SKM_baseball_bat_001"));
+	if (WeaponMeshRef.Succeeded())
+	{
+		WeaponMesh->SetSkeletalMesh(WeaponMeshRef.Object);
+	}
+	WeaponMesh->SetupAttachment(GetMesh(), TEXT("RightHandSocket"));
+}
