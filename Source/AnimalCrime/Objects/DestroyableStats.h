8ad568865042e338adb4fb15543c69c760ea41fc
soloplay <somnium1108@gmail.com>
2025-12-11 23:27:12 +0900

    feat: 파괴 가능한 오브젝트의 Stat 구조체

---
+++ Source/AnimalCrime/Objects/DestroyableStats.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "DestroyableStats.generated.h"
+
+USTRUCT()
+struct FDestroyableStats
+{
+	GENERATED_BODY()
+	
+	/** 물체의 최대 체력 */
+	UPROPERTY(EditAnywhere)
+	float MaxHp = 100.0f;
+	
+	/** 물체의 현재 체력 */
+	UPROPERTY(EditAnywhere)
+	float CurrentHp = 100.0f;
+	
+	/** 물체의 방어력 */
+	UPROPERTY(EditAnywhere)
+	float Armor = 5;
+	
+	/** 파괴 시 획득 가능한 점수 */
+	UPROPERTY(EditAnywhere)
+	float ScorePoint = 10;
+};
