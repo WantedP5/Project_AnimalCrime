aa65545b55fe7d4ededcbe075aa887f2e270f9ed
soloplay <somnium1108@gmail.com>
2025-12-14 03:46:24 +0900

    feat: AI 피격 로직 구현
    - 피격 시 애니메이션 4방향 구현
    - 도망갈 위치 구하는 로직 구현
    - 여러 번 피격시 움직이는 로직 구현

--- Source/AnimalCrime/Character/ACCitizen.h
+++ Source/AnimalCrime/Character/ACCitizen.h
@@ -11,6 +11,14 @@ class ANIMALCRIME_API AACCitizen : public ACharacter
 {
 	GENERATED_BODY()
 
+	enum class State
+	{
+		Front,
+		Back,
+		Left,
+		Right
+	};
+	
 #pragma region 특수 맴버 함수
 public:
 	AACCitizen();
@@ -20,18 +28,49 @@ public:
 public:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
-	
+
+	void PlayDamagedMontage(const FVector& Attack);
 	virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser) override;
 #pragma endregion
 	
 public:	// 좌표
 	FVector GetNextPosition() const;
 	
+	
+	FVector GetRunPosition(const FVector& Attack) const;
+	
+	UFUNCTION(BlueprintCallable)
+	int32 GetDamageFlag() {return DamagedFlag;}
+	
+	void OnDamaged();
+	
+	void OnArrive();
+	
+	float GetLastHitTime() const;
+	
+	UFUNCTION(NetMulticast, Reliable)
+	void MulticastOnPlayMontage(const FVector& Attack);
 #pragma region 맴버 변수
 public:
 	
 protected:
 	
-private:
+public:
+	UPROPERTY(meta=(AllowPrivateAccess=true))
+	int32 DamagedFlag;
+	
+	UPROPERTY(meta=(AllowPrivateAccess=true))
+	float LastHitTime;
+	
+	UPROPERTY(EditAnywhere, meta=(AllowPrivateAccess=true))
+	TObjectPtr<class UAnimMontage> DamagedMontage;
+	
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	TObjectPtr<class USkeletalMeshComponent> HeadMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	TObjectPtr<class USkeletalMeshComponent> TopMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
+	TObjectPtr<class USkeletalMeshComponent> BottomMesh;
 #pragma endregion
 };
