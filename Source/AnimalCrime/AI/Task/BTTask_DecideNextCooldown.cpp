377f9e42a8ade1ecc2cb6e9e27a3d544e7d0b7f4
soloplay <somnium1108@gmail.com>
2025-12-14 03:48:16 +0900

    feat: 랜덤 쿨타임 Task 구현.
    - 실행이 끝나고 난 후 다음 쿨타임 값을 Blackboard에 저장

---
+++ Source/AnimalCrime/AI/Task/BTTask_DecideNextCooldown.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_DecideNextCooldown.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_DecideNextCooldown::UBTTask_DecideNextCooldown()
+{
+}
+
+EBTNodeResult::Type UBTTask_DecideNextCooldown::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::ExecuteTask(OwnerComp, NodeMemory);
+	
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	if (AIController == nullptr)
+	{
+		return EBTNodeResult::Type::Failed;
+	}
+	
+	UBlackboardComponent* BBComp = AIController->GetBlackboardComponent();
+	if (BBComp == nullptr)
+	{
+		return EBTNodeResult::Type::Failed;
+	}
+	
+	float NextCooldown = FMath::RandRange(1, 10);
+	BBComp->SetValueAsFloat("NextCooldown", NextCooldown);
+	
+	return EBTNodeResult::Type::Succeeded;
+}
---
+++ Source/AnimalCrime/AI/Task/BTTask_DecideNextCooldown.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_DecideNextCooldown.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMALCRIME_API UBTTask_DecideNextCooldown : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+public:
+	UBTTask_DecideNextCooldown();
+	
+public:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
