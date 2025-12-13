18c96a6cf35406a05077cfd51d7ccb364ba0de0a
soloplay <somnium1108@gmail.com>
2025-12-14 03:47:05 +0900

    add: 애니메이션 Task 추가

---
+++ Source/AnimalCrime/AI/Task/BTTask_PerformAction.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/Task/BTTask_PerformAction.h"
+
+UBTTask_PerformAction::UBTTask_PerformAction()
+{
+}
---
+++ Source/AnimalCrime/AI/Task/BTTask_PerformAction.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_PlayAnimation.h"
+#include "BTTask_PerformAction.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMALCRIME_API UBTTask_PerformAction : public UBTTask_PlayAnimation
+{
+	GENERATED_BODY()
+	
+public:
+	UBTTask_PerformAction();
+	
+public:
+	
+};
