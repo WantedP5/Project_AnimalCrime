093ce771f22dc25bb1e6f2c75c2fdab58cd86730
soloplay <somnium1108@gmail.com>
2025-12-15 15:14:46 +0900

    add: 게임 인스턴스 생서

---
+++ Source/AnimalCrime/Game/ACMainGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ACMainGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMALCRIME_API UACMainGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
