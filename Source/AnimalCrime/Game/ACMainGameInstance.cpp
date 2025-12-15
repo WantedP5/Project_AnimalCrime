9de1b5765042695097c9e49536e6e36a02d76427
soloplay <somnium1108@gmail.com>
2025-12-15 16:55:32 +0900

    feat: 게임 Level 로드하는 로직 구현
    - 기존 GameMode에 있던 로직을 GameInstance로 변경

--- Source/AnimalCrime/Game/ACMainGameInstance.cpp
+++ Source/AnimalCrime/Game/ACMainGameInstance.cpp
@@ -3,10 +3,50 @@
 
 #include "Game/ACMainGameInstance.h"
 
+#pragma region 생성자 
 UACMainGameInstance::UACMainGameInstance()
 {
 	UE_LOG(LogTemp, Warning, TEXT("UACMainGameInstance::UACMainGameInstance"));
+}
+#pragma endregion
+
+#pragma region 엔진 제공 함수
+void UACMainGameInstance::Init()
+{
+	Super::Init();
+	
 	CurrentMapType = EMapType::Game;
+	
+	LobbyMapName = TEXT("LobbyMap");
+	GameMapName = TEXT("HenaMap");
+	
+	UE_LOG(LogTemp, Warning, TEXT("UACMainGameInstance::Init"));
+}
+
+void UACMainGameInstance::Shutdown()
+{
+	Super::Shutdown();
+	
+	UE_LOG(LogTemp, Warning, TEXT("UACMainGameInstance::Shutdown"));
+}
+
+void UACMainGameInstance::OnStart()
+{
+	Super::OnStart();
+	
+	UE_LOG(LogTemp, Warning, TEXT("UACMainGameInstance::OnStart"));
+}
+#pragma endregion
+
+#pragma region Map Level
+void UACMainGameInstance::LoadLobbyMap()
+{
+	UpdateMap(EMapType::Lobby);
+}
+
+void UACMainGameInstance::LoadGameMap()
+{
+	UpdateMap(EMapType::Game);
 }
 
 EMapType UACMainGameInstance::GetMapType() const
@@ -16,15 +56,25 @@ EMapType UACMainGameInstance::GetMapType() const
 
 void UACMainGameInstance::UpdateMap(const EMapType InMapType)
 {
+	if (CurrentMapType == InMapType)
+	{
+		UE_LOG(LogTemp, Error, TEXT("CurrentMapType == InMapType"));
+		return ;
+	}
+
+	switch (InMapType)
+	{
+	case EMapType::Lobby:
+		GetWorld()->ServerTravel(LobbyMapName);
+		break;
+	case EMapType::Game:
+		GetWorld()->ServerTravel(GameMapName);
+		break;
+	default:
+		break;
+	}
+	
+	// 현재 맵을 변경된 맵으로 Update
 	CurrentMapType = InMapType;
 }
-
-void UACMainGameInstance::UpdateLobbyMap()
-{
-	CurrentMapType = EMapType::Lobby;
-}
-
-void UACMainGameInstance::UpdateGameMap()
-{
-	CurrentMapType = EMapType::Game;
-}
+#pragma endregion
--- Source/AnimalCrime/Game/ACMainGameInstance.h
+++ Source/AnimalCrime/Game/ACMainGameInstance.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -25,15 +25,38 @@ class ANIMALCRIME_API UACMainGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
+#pragma region 생성자 
 public:
 	UACMainGameInstance();
+#pragma endregion
 	
+#pragma region 엔진 제공 함수
 public:
+	virtual void Init() override;
+	virtual void Shutdown() override;
+	virtual void OnStart() override;
+#pragma endregion
+
+#pragma region Map Level 
+public:
+	void LoadLobbyMap();
+	void LoadGameMap();
+
 	EMapType GetMapType() const;
-	void UpdateMap(EMapType InMapType);
-	void UpdateLobbyMap();
-	void UpdateGameMap();
 	
 private:
+	void UpdateMap(EMapType InMapType);
+#pragma endregion
+
+#pragma region Map Level 관련 맴버 변수 
+private:
+	UPROPERTY(EditAnywhere, meta=(AllowPrivateAccess = true))
 	EMapType CurrentMapType;
+	
+	UPROPERTY(EditAnywhere, meta=(AllowPrivateAccess = true))
+	FString LobbyMapName;
+	
+	UPROPERTY(EditAnywhere, meta=(AllowPrivateAccess = true))
+	FString GameMapName;
+#pragma endregion
 };
