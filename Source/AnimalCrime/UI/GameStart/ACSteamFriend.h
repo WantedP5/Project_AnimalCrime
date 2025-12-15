d14af0f1ef4066cd04f96199b7c2281088944e75
sangyun lee <son5165@naver.com>
2025-12-15 18:01:25 +0900

    refactor: 스팀 친구 초대 C++로 로직 수정

--- Source/AnimalCrime/UI/GameStart/ACSteamFriend.h
+++ Source/AnimalCrime/UI/GameStart/ACSteamFriend.h
@@ -11,16 +11,24 @@ UCLASS()
 class ANIMALCRIME_API UACSteamFriend : public UUserWidget
 {
 	GENERATED_BODY()
-	
+protected:
+	virtual void NativeConstruct() override;
+
 public:
 	UFUNCTION(BlueprintCallable)
 	void UpdateFriend(const FBPFriendInfo& NewFriendDataInfo);
+
+protected:
+	UFUNCTION()
+	void OnInviteButtonClicked();
 
 protected:
 	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
 	TObjectPtr<class UTextBlock> PlayerNameText;
 	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
 	TObjectPtr<class UImage> AvatarImage;
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	TObjectPtr<class UButton> InviteButton;
 
 protected:
 	//!< FriendDataInfo 변수
