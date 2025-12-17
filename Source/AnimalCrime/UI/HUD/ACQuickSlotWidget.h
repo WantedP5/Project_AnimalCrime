3ffb77d803a4d9cab719934c9fca5aa446ec8228
GyeonHee <hyungi4833@gmail.com>
2025-12-17 16:06:38 +0900

    feat: 퀵슬롯 위젯 추가

---
+++ Source/AnimalCrime/UI/HUD/ACQuickSlotWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/ACCustomWidget.h"
+#include "ACQuickSlotWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMALCRIME_API UACQuickSlotWidget : public UACCustomWidget
+{
+	GENERATED_BODY()
+	
+};
