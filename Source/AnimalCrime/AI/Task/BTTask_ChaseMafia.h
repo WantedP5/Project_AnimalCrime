18784582397695bef00ac5b64637f5f1d421a548
soloplay <somnium1108@gmail.com>
2025-12-27 00:15:10 +0900

    add: 시민 공격 Task 추가

---
+++ Source/AnimalCrime/AI/Task/BTTask_ChaseMafia.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_ChaseMafia.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMALCRIME_API UBTTask_ChaseMafia : public UBTTaskNode
+{
+	GENERATED_BODY()
+};
---
+++ Source/AnimalCrime/AI/Task/BTTask_LookAtTarget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_LookAtTarget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMALCRIME_API UBTTask_LookAtTarget : public UBTTaskNode
+{
+	GENERATED_BODY()
+};
