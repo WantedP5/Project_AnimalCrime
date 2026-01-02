0f9e8bb718cc9ec44c2fc338ee0cd7383e95337e
soloplay <somnium1108@gmail.com>
2026-01-02 20:42:33 +0900

    test: Cpp로 테스트
    - 단일mesh 은 성공했지만,  다중 mesh는 실패

---
+++ Source/AnimalCrime/Objects/ACBush.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ACDestroyableObject.h"
+#include "GameFramework/Actor.h"
+#include "ACBush.generated.h"
+
+UCLASS()
+class ANIMALCRIME_API AACBush : public AACDestroyableObject
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AACBush();
+
+public:
+	virtual void BeginPlay() override;
+	virtual void PostNetInit() override;
+	virtual void PostInitializeComponents() override;
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY()
+	TObjectPtr<UStaticMeshComponent> MeshComp;
+};
---
+++ Source/AnimalCrime/Objects/ACCar6.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ACDestroyableObject.h"
+#include "ACCar6.generated.h"
+
+UCLASS()
+class ANIMALCRIME_API AACCar6 : public AACDestroyableObject
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AACCar6();
+
+public:
+	virtual void BeginPlay() override;
+	virtual void PostNetInit() override;
+	virtual void PostInitializeComponents() override;
+	virtual void Tick(float DeltaTime) override;
+};
