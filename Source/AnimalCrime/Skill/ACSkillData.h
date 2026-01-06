5722332fb0cf7972f83614a9fe749d46e72a3f94
soloplay <somnium1108@gmail.com>
2026-01-06 22:18:42 +0900

    add: 캐릭터 스킬 데이터 추가

---
+++ Source/AnimalCrime/Skill/ACSkillData.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ACSkillData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMALCRIME_API UACSkillData : public UDataAsset
+{
+	GENERATED_BODY()
+};
