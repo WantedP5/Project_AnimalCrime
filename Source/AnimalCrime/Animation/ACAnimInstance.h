7300e2ea1a423561ffa5e118d5cd616c3e8c5d34
soloplay <somnium1108@gmail.com>
2026-01-06 16:49:57 +0900

    feat: animinstance 일단 생성

---
+++ Source/AnimalCrime/Animation/ACAnimInstance.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ACAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMALCRIME_API UACAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+};
